982de6417a945276456b6dbd55a1ddf522da212c
Axe <seamac@gmail.com>
2026-03-16 15:51:09 +0000

    Add comptime bounded array coverage

--- src/compiler/mod.test.zig
+++ src/compiler/mod.test.zig
@@ -6952,6 +6952,76 @@ test "compiler const eval generic calls can return value-generic alias values" {
     try testing.expectEqual(@as(i128, 42), try consteval.values[ret_stmt.value.?.index()].?.integer.toInt(i128));
 }
 
+test "compiler const eval resolves array-size generic aliases in comptime blocks" {
+    const source_text =
+        \\type BoundedArray(comptime T: type, comptime N: u256) = [T; N];
+        \\
+        \\pub fn run() -> u256 {
+        \\    return comptime {
+        \\        let values: BoundedArray<u256, 3> = [1, 2, 3];
+        \\        values[1];
+        \\    };
+        \\}
+    ;
+
+    var compilation = try compileText(source_text);
+    defer compilation.deinit();
+
+    const module = compilation.db.sources.module(compilation.root_module_id);
+    const ast_file = try compilation.db.astFile(module.file_id);
+    const function = ast_file.item(ast_file.root_items[1]).Function;
+    const body = ast_file.body(function.body);
+    const ret_stmt = ast_file.statement(body.statements[0]).Return;
+    const comptime_expr = ast_file.expression(ret_stmt.value.?).Comptime;
+    const comptime_body = ast_file.body(comptime_expr.body);
+    const values_decl = ast_file.statement(comptime_body.statements[0]).VariableDecl;
+    const index_stmt = ast_file.statement(comptime_body.statements[1]).Expr;
+
+    const typecheck = try compilation.db.typeCheck(compilation.root_module_id, .{ .item = ast_file.root_items[1] });
+    try testing.expectEqual(compiler.sema.TypeKind.array, typecheck.exprType(values_decl.value.?).kind());
+    try testing.expectEqual(compiler.sema.TypeKind.integer, typecheck.exprType(index_stmt.expr).kind());
+
+    const consteval = try compilation.db.constEval(compilation.root_module_id);
+    try testing.expectEqual(@as(i128, 2), try consteval.values[ret_stmt.value.?.index()].?.integer.toInt(i128));
+}
+
+test "compiler const eval generic calls can return array-size generic alias values" {
+    const source_text =
+        \\type BoundedArray(comptime T: type, comptime N: u256) = [T; N];
+        \\
+        \\comptime fn make_bounded(comptime T: type, comptime N: u256, a: T, b: T, c: T) -> BoundedArray<T, N> {
+        \\    return [a, b, c];
+        \\}
+        \\
+        \\pub fn run() -> u256 {
+        \\    return comptime {
+        \\        let values = make_bounded(u256, 3, 4, 5, 6);
+        \\        values[2];
+        \\    };
+        \\}
+    ;
+
+    var compilation = try compileText(source_text);
+    defer compilation.deinit();
+
+    const module = compilation.db.sources.module(compilation.root_module_id);
+    const ast_file = try compilation.db.astFile(module.file_id);
+    const function = ast_file.item(ast_file.root_items[2]).Function;
+    const body = ast_file.body(function.body);
+    const ret_stmt = ast_file.statement(body.statements[0]).Return;
+    const comptime_expr = ast_file.expression(ret_stmt.value.?).Comptime;
+    const comptime_body = ast_file.body(comptime_expr.body);
+    const values_decl = ast_file.statement(comptime_body.statements[0]).VariableDecl;
+    const index_stmt = ast_file.statement(comptime_body.statements[1]).Expr;
+
+    const typecheck = try compilation.db.typeCheck(compilation.root_module_id, .{ .item = ast_file.root_items[2] });
+    try testing.expectEqual(compiler.sema.TypeKind.array, typecheck.exprType(values_decl.value.?).kind());
+    try testing.expectEqual(compiler.sema.TypeKind.integer, typecheck.exprType(index_stmt.expr).kind());
+
+    const consteval = try compilation.db.constEval(compilation.root_module_id);
+    try testing.expectEqual(@as(i128, 6), try consteval.values[ret_stmt.value.?.index()].?.integer.toInt(i128));
+}
+
 test "compiler db breaks same-key const eval recursion with unknown sentinel" {
     const source_text =
         \\comptime fn loop() -> u256 {
