87638cfadf0cd692b5a0899261a540578da6ddc9
Axe <seamac@gmail.com>
2026-03-22 21:19:18 +0000

    More docs updates

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -26,7 +26,6 @@ const config: Config = {
   projectName: 'Ora', // Usually your repo name.
 
   onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
@@ -64,6 +63,9 @@ const config: Config = {
 
   markdown: {
     mermaid: true,
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
   },
 
   themes: ['@docusaurus/theme-mermaid'],
