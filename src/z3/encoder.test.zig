c1d32b043f14f8820a229b49a99a863d4f2c2ad3
Axe <seamac@gmail.com>
2026-03-26 23:15:14 +0000

    Cover known-callee swapped while SMT exactness

--- src/z3/encoder.test.zig
+++ src/z3/encoder.test.zig
@@ -13834,6 +13834,120 @@ test "known pure callee finite two-iteration scf.while return encodes exactly" {
     try testing.expectEqual(@as(z3.Z3_lbool, z3.Z3_L_FALSE), solver.check());
 }
 
+test "known pure callee canonical unsigned swapped-compare scf.while return encodes exactly" {
+    var z3_ctx = try Context.init(testing.allocator);
+    defer z3_ctx.deinit();
+
+    var encoder = Encoder.init(&z3_ctx, testing.allocator);
+    defer encoder.deinit();
+
+    const mlir_ctx = mlir.oraContextCreate();
+    defer mlir.oraContextDestroy(mlir_ctx);
+    loadAllDialects(mlir_ctx);
+    _ = mlir.oraDialectRegister(mlir_ctx);
+
+    const loc = mlir.oraLocationUnknownGet(mlir_ctx);
+    const i256_ty = mlir.oraIntegerTypeCreate(mlir_ctx, 256);
+
+    const helper_attrs = [_]mlir.MlirNamedAttribute{
+        namedAttr(mlir_ctx, "sym_name", mlir.oraStringAttrCreate(mlir_ctx, stringRef("unsignedSwappedCmpWhileReturn"))),
+    };
+    const helper_param_types = [_]mlir.MlirType{i256_ty};
+    const helper_param_locs = [_]mlir.MlirLocation{loc};
+    const helper = mlir.oraFuncFuncOpCreate(mlir_ctx, loc, &helper_attrs, helper_attrs.len, &helper_param_types, &helper_param_locs, helper_param_types.len);
+    const body = mlir.oraFuncOpGetBodyBlock(helper);
+
+    const init_op = mlir.oraArithConstantOpCreate(mlir_ctx, loc, i256_ty, mlir.oraIntegerAttrCreateI64FromType(i256_ty, 0));
+    const delta_op = mlir.oraArithConstantOpCreate(mlir_ctx, loc, i256_ty, mlir.oraIntegerAttrCreateI64FromType(i256_ty, 3));
+    mlir.oraBlockAppendOwnedOperation(body, init_op);
+    mlir.oraBlockAppendOwnedOperation(body, delta_op);
+
+    const bound = mlir.oraBlockGetArgument(body, 0);
+    const init_vals = [_]mlir.MlirValue{mlir.oraOperationGetResult(init_op, 0)};
+    const result_types = [_]mlir.MlirType{i256_ty};
+    const while_op = mlir.oraScfWhileOpCreate(mlir_ctx, loc, &init_vals, init_vals.len, &result_types, result_types.len);
+    const before_block = mlir.oraScfWhileOpGetBeforeBlock(while_op);
+    const after_block = mlir.oraScfWhileOpGetAfterBlock(while_op);
+    _ = mlir.mlirBlockAddArgument(before_block, i256_ty, loc);
+    _ = mlir.mlirBlockAddArgument(after_block, i256_ty, loc);
+    const before_arg = mlir.oraBlockGetArgument(before_block, 0);
+    const after_arg = mlir.oraBlockGetArgument(after_block, 0);
+
+    const cmp_op = mlir.oraArithCmpIOpCreate(mlir_ctx, loc, 8, bound, before_arg); // bound > current
+    mlir.oraBlockAppendOwnedOperation(before_block, cmp_op);
+    mlir.oraBlockAppendOwnedOperation(before_block, mlir.oraScfConditionOpCreate(
+        mlir_ctx,
+        loc,
+        mlir.oraOperationGetResult(cmp_op, 0),
+        &[_]mlir.MlirValue{before_arg},
+        1,
+    ));
+
+    const next_op = mlir.oraArithAddIOpCreate(mlir_ctx, loc, after_arg, mlir.oraOperationGetResult(delta_op, 0));
+    mlir.oraBlockAppendOwnedOperation(after_block, next_op);
+    mlir.oraBlockAppendOwnedOperation(after_block, mlir.oraScfYieldOpCreate(
+        mlir_ctx,
+        loc,
+        &[_]mlir.MlirValue{mlir.oraOperationGetResult(next_op, 0)},
+        1,
+    ));
+    mlir.oraBlockAppendOwnedOperation(body, while_op);
+    mlir.oraBlockAppendOwnedOperation(body, mlir.oraReturnOpCreate(
+        mlir_ctx,
+        loc,
+        &[_]mlir.MlirValue{mlir.oraOperationGetResult(while_op, 0)},
+        1,
+    ));
+
+    try encoder.registerFunctionOperation(helper);
+
+    const caller_bound = mlir.oraVariablePlaceholderOpCreate(mlir_ctx, loc, stringRef("callerUnsignedSwappedCmpBound"), i256_ty);
+    const call = mlir.oraFuncCallOpCreate(
+        mlir_ctx,
+        loc,
+        stringRef("unsignedSwappedCmpWhileReturn"),
+        &[_]mlir.MlirValue{mlir.oraOperationGetResult(caller_bound, 0)},
+        1,
+        &[_]mlir.MlirType{i256_ty},
+        1,
+    );
+    const encoded = try encoder.encodeOperation(call);
+    try testing.expect(!encoder.isDegraded());
+
+    const init_ast = try encoder.encodeOperation(init_op);
+    const bound_ast = try encoder.encodeOperation(caller_bound);
+    const delta_ast = try encoder.encodeOperation(delta_op);
+    const sort = z3.Z3_get_sort(z3_ctx.ctx, bound_ast);
+    const zero = z3.Z3_mk_unsigned_int64(z3_ctx.ctx, 0, sort);
+    const one = z3.Z3_mk_unsigned_int64(z3_ctx.ctx, 1, sort);
+    const init_ge_bound = z3.Z3_mk_bvuge(z3_ctx.ctx, init_ast, bound_ast);
+    const distance = z3.Z3_mk_ite(
+        z3_ctx.ctx,
+        init_ge_bound,
+        zero,
+        z3.Z3_mk_bv_sub(z3_ctx.ctx, bound_ast, init_ast),
+    );
+    const distance_is_zero = z3.Z3_mk_eq(z3_ctx.ctx, distance, zero);
+    const three = z3.Z3_mk_unsigned_int64(z3_ctx.ctx, 3, sort);
+    const step_count = z3.Z3_mk_ite(
+        z3_ctx.ctx,
+        distance_is_zero,
+        zero,
+        z3.Z3_mk_bv_add(
+            z3_ctx.ctx,
+            z3.Z3_mk_bv_udiv(z3_ctx.ctx, z3.Z3_mk_bv_sub(z3_ctx.ctx, distance, one), three),
+            one,
+        ),
+    );
+    const total_delta = try encoder.encodeArithmeticOp(.Mul, step_count, delta_ast);
+    const expected = try encoder.encodeArithmeticOp(.Add, init_ast, total_delta);
+
+    var solver = try Solver.init(&z3_ctx, testing.allocator);
+    defer solver.deinit();
+    solver.assert(z3.Z3_mk_not(z3_ctx.ctx, z3.Z3_mk_eq(z3_ctx.ctx, encoded, expected)));
+    try testing.expectEqual(@as(z3.Z3_lbool, z3.Z3_L_FALSE), solver.check());
+}
+
 test "unsigned mul overflow check proves bounded constant multiplier safe" {
     var z3_ctx = try Context.init(testing.allocator);
     defer z3_ctx.deinit();
